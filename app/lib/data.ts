0b3426fa81e7b2038a52263c6f94e30e85aa7b00
MateoSantar <136135064+MateoSantar@users.noreply.github.com>
2025-10-28 16:34:12 -0300

    cards added

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,7 +8,6 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
-
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
